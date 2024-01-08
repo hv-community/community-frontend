56e098bab04b19baeb4ce9a33f799346e38978d6
GOOMBANGEE <120004316+GOOMBANGEE@users.noreply.github.com>
2024-01-09 07:59:17 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        ...
+      </button>
+    </>
+  );
+}
